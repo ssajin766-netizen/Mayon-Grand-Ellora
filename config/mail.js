4839db80ddcaed88e4b796fc17e6d459dcf540a8
Sajin <ssajin766@gmail.com>
2026-07-13 12:09:44 +0530

    Configure Brevo SMTP

--- config/mail.js
+++ config/mail.js
@@ -4,29 +4,31 @@ const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
+    requireTLS: true,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
 
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+
     logger: true,
     debug: true
 });
 
 transporter.verify((err) => {
-
     console.log("================================");
 
     if (err) {
-        console.log("MAIL VERIFY FAILED");
         console.error(err);
     } else {
         console.log("MAIL SERVER READY");
     }
 
     console.log("================================");
-
 });
 
 module.exports = transporter;
